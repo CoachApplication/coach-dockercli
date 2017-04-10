1a02fb112f25473b3703cffd08ce8412486c2e06
james.nesbitt@wunder.io <james.nesbitt@wunder.io>
2017-04-10 10:21:33 +0300

    initial configwrapper config structs

---
+++ configwrapper/config.go
@@ -0,0 +1,4 @@
+package configwrapper
+
+type ConfigSettings struct {
+}
---
+++ configwrapper/config_yml_test.go
@@ -0,0 +1,10 @@
+package configwrapper_test
+
+import (
+	dcli_cw "github.com/CoachApplication/handler-dockercli/configwrapper"
+)
+
+var cdBytes []byte = []byte(`
+`)
+
+var cd = dcli_cw.ConfigSettings{}
